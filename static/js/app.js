5a85a2da6b8460c9aa0cba96aed5dec5e776a51d
xuemeiwei <xuemeiwei1226@gmail.com>
2016-03-06 09:31:03 -0800

    web_client_id change

--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '912668979886-bkh62upoupla82j4vpj80bl50m1t762u.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
